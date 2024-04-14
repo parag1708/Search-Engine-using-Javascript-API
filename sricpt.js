3b0989c1e796d1c50da3399cfcf98c0bf4483694
Parag1708 <paragsharma1708@gmail.com>
2024-04-14 17:03:48 +0530

    first commit

---
+++ sricpt.js
@@ -0,0 +1,50 @@
+ const accesskey="psM_ifoWQooJ07oll4chGIh-a3GFMpsW_YoAoaBS3RQ";
+ 
+ const searchForm=document.getElementById("search-form");
+ const searchBox=document.getElementById("search-box");
+ const searchresult=document.getElementById("search-result");
+ const showMoreBtn=document.getElementById("show-more-btn");
+
+
+let keyword="";
+let page=1;
+
+ async function searchImages(){
+    keyword = searchBox.value;
+    const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
+    // const url ='https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+    const response=await fetch(url);
+    const data =await response.json();
+
+    if(page===1){
+      searchresult.innerHTML="";
+    }
+
+
+   //  console.log(data);
+
+   const results =data.results;
+   results.map((result)=>{
+      const image=document.createElement("img");
+      image.src= result.urls.small;
+      const imageLink=document.createElement("a");
+      imageLink.href=result.links.html;
+      imageLink.target="_blank";
+
+      imageLink.appendChild(image);
+      searchresult.appendChild(imageLink);
+   })
+   showMoreBtn.style.display="block";
+
+ }
+
+ searchForm.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    page=1;
+    searchImages();
+ })
+
+ showMoreBtn.addEventListener("click",()=>{
+   page++;
+   searchImages();
+ })
